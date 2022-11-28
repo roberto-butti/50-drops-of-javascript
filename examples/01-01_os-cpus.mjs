48b71d15f9c208c34b6e0c6153aa5db59c18862a
Roberto Butti <roberto.butti@gmail.com>
2022-11-28 23:52:16 +0100

    rename examples,  js to mjs

--- examples/01-01_os-cpus.mjs
+++ examples/01-01_os-cpus.mjs
@@ -1,6 +1,5 @@
-const os = require('os')
-const cpus = os.cpus()
-cpus.forEach(cpu => {
+import { cpus } from 'os'
+cpus().forEach(cpu => {
   console.info(cpu.model + ' speed: ' + cpu.speed)
   console.table(cpu.times)
 })
