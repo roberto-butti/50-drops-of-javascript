458ec0e06aeda62e8f04b353c40333a7d5b6d35c
Roberto Butti <roberto.butti@gmail.com>
2022-12-03 10:55:15 +0100

    Create 01-06_os-totalmem.mjs

---
+++ examples/01-06_os-totalmem.mjs
@@ -0,0 +1,19 @@
+import { totalmem } from 'os'
+const m = totalmem()
+console.log(
+  'Hi, the total memory is %d bytes',
+  m
+)
+console.log(
+  'Hi, the total memory is %d kilobytes',
+  m / 1024
+)
+console.log(
+  'Hi, the total memory is %d megabytes',
+  m / 1024 / 1024
+)
+
+console.log(
+  'Hi, the total memory is %d gigabytes',
+  m / 1024 / 1024 / 1024
+)
